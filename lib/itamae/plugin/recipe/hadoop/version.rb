4c1bd7127e2241c7689c967daece3790979e4162
y-kumazaki <kumazaki@lab.acs-jp.com>
2021-04-09 14:40:23 +0900

    add support for hadoop-3.2.1

--- lib/itamae/plugin/recipe/hadoop/version.rb
+++ lib/itamae/plugin/recipe/hadoop/version.rb
@@ -2,12 +2,12 @@ module Itamae
   module Plugin
     module Recipe
       module Hadoop
-        VERSION = "0.1.0"
+        VERSION = "0.1.1"
 
         HADOOP_VERSION = [
-          HADOOP_VERSION_MAJOR = '2',
-          HADOOP_VERSION_MINOR = '8',
-          HADOOP_VERSION_REVISION = '5'
+          HADOOP_VERSION5_MAJOR = '3',
+          HADOOP_VERSION_MINOR = '2',
+          HADOOP_VERSION_REVISION = '1'
         ].join('.')
       
       end

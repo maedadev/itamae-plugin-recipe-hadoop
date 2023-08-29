0454e089e68b02d4bf2a8fd1fa374d81a5aeb2f9
yuichiro.asahi <yuichiro.asahi@ulsystems.co.jp>
2023-08-29 10:35:45 +0900

    add support for hadoop-3.3.3

--- lib/itamae/plugin/recipe/hadoop/version.rb
+++ lib/itamae/plugin/recipe/hadoop/version.rb
@@ -2,12 +2,12 @@ module Itamae
   module Plugin
     module Recipe
       module Hadoop
-        VERSION = "0.1.2"
+        VERSION = "0.1.3"
 
         HADOOP_VERSION = [
           HADOOP_VERSION5_MAJOR = '3',
-          HADOOP_VERSION_MINOR = '2',
-          HADOOP_VERSION_REVISION = '1'
+          HADOOP_VERSION_MINOR = '3',
+          HADOOP_VERSION_REVISION = '3'
         ].join('.')
       
       end
